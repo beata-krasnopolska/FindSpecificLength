cadedc4baa860cbf526c05b04ef0f7376ec38283
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2019-02-06 21:30:42 +0100

    Add project files.

---
+++ FindSpecificLengthLINQ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindSpecificLengthLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program finds the strings with a specific minimum length");
+            List<string> Cities = new List<string> { "Rzeszów", "Kraków", "Lublin", "Łódź", "Warszawa", "Gdańsk" };
+
+            Console.WriteLine("Enter minimum string length");
+            int wordLength = int.Parse(Console.ReadLine());
+
+            var myCities = from city in Cities
+                           where city.Length >= wordLength
+                           orderby city
+                           select city;
+
+            Console.WriteLine("Words with a minimum length of {0} characters are:", wordLength);
+            foreach (var city in myCities)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
